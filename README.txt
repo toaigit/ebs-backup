f5fc4bf2ead3ec6fe9884687728cb02f83dcb5a2
Toai Vo <toaivo@stanford.edu>
2023-04-18 13:34:10 -0700

    Intial Version

---
+++ README.txt
@@ -0,0 +1,2 @@
+This will create a lifecycle manager for daily snapshot backup with 7 days retention.
+Any volume with the tag backup_policy = "daily" will be backup daily.
---
+++ main.tf
@@ -0,0 +1,85 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["dlm.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "dlm_lifecycle_role" {
+  name               = "dlm-lifecycle-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "dlm_lifecycle" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:CreateSnapshot",
+      "ec2:CreateSnapshots",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeInstances",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeSnapshots",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:CreateTags"]
+    resources = ["arn:aws:ec2:*::snapshot/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "dlm_lifecycle" {
+  name   = "my-dlm-lifecycle-policy"
+  role   = aws_iam_role.dlm_lifecycle_role.id
+  policy = data.aws_iam_policy_document.dlm_lifecycle.json
+}
+
+resource "aws_dlm_lifecycle_policy" "daily7days" {
+  description        = "Custom DLM lifecycle policy"
+  execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
+  state              = "ENABLED"
+
+  tags = {
+     Name = "daily7days policy"
+     Creator = "Toai Vo"
+     Purpose = "Daily Snapshot with 7 days retention"
+     }
+  policy_details {
+    resource_types = ["VOLUME"]
+
+    schedule {
+      name = "Daily snapshots with 7 days retention"
+
+      create_rule {
+        interval      = 24
+        interval_unit = "HOURS"
+        times         = ["23:45"]
+      }
+
+      retain_rule {
+        count = 7
+      }
+
+      tags_to_add = {
+        SnapshotCreator = "created by terraform"
+      }
+
+      copy_tags = false
+    }
+
+    target_tags = {
+      backup_policy = "daily"
+    }
+  }
+}
